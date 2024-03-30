c34db68836f50dcb134007ce7735de0931e94191
Shoykot Amanat <shoykot.amanat@gmail.com>
2024-03-31 02:40:24 +0600

    added dropdown script

---
+++ dropdown.py
@@ -0,0 +1,23 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.select import Select
+
+class DemoDropdownSingleSelect():
+    def demo_dropdown(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.salesforce.com/in/form/signup/freetrial-sales/?d=topnav2-btn-ft")
+        dropdown = driver.find_element(By.NAME, "UserTitle")
+
+        dd = Select(dropdown)
+        dd.select_by_index(2)
+        time.sleep(2)
+        dd.select_by_value("CxO_General_Manager_ANZ")
+        time.sleep(3)
+        dd.select_by_visible_text("Customer Service Manager")
+        time.sleep(3)
+        driver.quit()
+ddd = DemoDropdownSingleSelect()
+ddd.demo_dropdown()
+
